61e89a2e2a138396f89f535bc00aef81af22cbd9
Mohamadreza Panahandeh <mdrezapanahandeh@gmail.com>
2026-02-13 21:47:49 +0330

    Update Teacher.cs

--- Teacher.cs
+++ Teacher.cs
@@ -9,6 +9,7 @@ public class Teacher : Person
     public required TeacherCode TeacherCode { get; init; }
     public required List<string>? Teachercourses { get; init; }
     public override string ToString()=>
-        base.ToString() + $" , tc : {TeacherCode} ";
+       base.ToString();
 }
+
 
