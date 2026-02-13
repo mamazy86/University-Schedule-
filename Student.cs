42eddc6bdf5153c71b8d5656b90598d57cb48532
Mohamadreza Panahandeh <mdrezapanahandeh@gmail.com>
2026-02-13 21:57:14 +0330

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -27,7 +27,8 @@ public class Student : Person
         }
     }
     public override string ToString() =>
-       base.ToString() + $" , avg : {StudentAverage}";
+       base.ToString() + $" , course average : {StudentAverage}";
 }
+
 
 
